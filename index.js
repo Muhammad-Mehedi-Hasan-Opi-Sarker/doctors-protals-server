b614b60038024323b5d70b44d9de0f22ebaf996f
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-06-28 22:32:37 +0600

    add services data

--- index.js
+++ index.js
@@ -16,7 +16,29 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zii4n.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri);
+
+async function run(){
+    
+    try{
+        await client.connect();
+        const dataCollection = client.db('doctors_protal_treatment').collection('services');
+
+        app.get('/service', async(req,res)=>{
+            const query={};
+            const cursor = dataCollection.find(query);
+            const result= await cursor.toArray();
+            res.send(result)
+
+        })
+
+    }
+    finally{
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
 
 
 app.listen(port, () => {
