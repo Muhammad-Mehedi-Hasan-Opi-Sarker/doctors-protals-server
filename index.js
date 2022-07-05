19f6fa9d9756ad9f3d3fe842d3691a4c0b236d01
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-07-05 15:19:58 +0600

    add user api

--- index.js
+++ index.js
@@ -23,19 +23,36 @@ async function run(){
         await client.connect();
         const dataCollection = client.db('doctors_protal_treatment').collection('services');
         const bookingCollection = client.db('doctors_protal_treatment').collection('booking');
+        const userCollection = client.db('doctors_protal_treatment').collection('user');
 
         app.get('/service', async(req,res)=>{
             const query={};
-            const cursor = dataCollection.find(query);
-            const result= await cursor.toArray();
-            res.send(result)
+            const cursor = await dataCollection.find(query).toArray();
+            res.send(cursor)
         })
+        // data pass for email for patient 
+        app.get('/booking', async (req, res) => {
+            const patientEmail = req.query.patientEmail;
+            const query = { patientEmail: patientEmail };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+          })
 
-        app.post('/booking', (req,res)=>{
+        app.post('/booking', async(req,res)=>{
             const booking = req.body;
             const result = await bookingCollection.insertOne(booking);
-            res.send(result);
-
+            return res.send(result);
+        })
+        app.put('/user/:email', async(req,res)=>{
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email:email};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+              };
+              const result = await userCollection.updateOne(filter,updateDoc, options);
+              res.send(result);
         })
 
     }
